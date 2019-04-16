5744d3648fae96c7321f9527ea683a9e9a4f07bd
QAQchengyu <1210670609@qq.com>
2019-04-16 15:42:40 +0800

    完成搜索页面

---
+++ src/components/myhead.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="search">
+    <div class="search_input">
+      <input type="text">
+      <div class="word">
+        <icon type="search" size="20"></icon>
+        <span>搜索</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.search {
+    background-color: #eb4450;
+    padding: 20rpx;
+
+    .search_input {
+        background-color: #fff;
+        border-radius: 3px;
+        width: 100%;
+        height: 60rpx;
+        position: relative;
+
+        .word {
+            position: absolute;
+            // display: flex;
+            // align-items: center;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%)
+        }
+    }
+
+}
+</style>
---
+++ src/utils/request.js
@@ -0,0 +1,31 @@
+// 负责封装请求
+var request = function (url, method = "get", data = {}, header = {}) {
+    return new Promise((resolve, reject) => {
+        wx.showLoading({
+            title: '数据加载中'
+        })
+        // 执行逻辑
+        wx.request({
+            url: url,
+            method: method,
+            data: data,
+            header: header,
+            success: (res) => {
+                wx.hideLoading()
+                // this 的指向
+                resolve(res)
+            },
+            fail: function (res) {
+                reject(res)
+            }
+        })
+    })
+}
+
+// 扩展封装一上get的方法
+request.get = function (url, data) {
+    return request(url, "get", data, {})
+}
+
+// 暴露方法
+export default request
