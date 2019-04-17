f7954d4d96d03c5152f5bff83e5ed666750a35da
QAQchengyu <1210670609@qq.com>
2019-04-17 15:39:26 +0800

    详情页面的购物车功能

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-    购物车
+    <!-- 收货地址 -->
+    <div class="address">
+      <!-- 没有选择地址的状态 -->
+      <div v-if="address != null ? true : false" class="no-address">
+        <div class="word">请选择收货地址</div>
+        <div class="iright">
+          <span class="iconfont iconright"></span>
+        </div>
+      </div>
+      <!-- 选择地址后的状态 -->
+      <div v-else class="yes-address">
+        <div class="uname">
+          <div class="name">
+            收件人： 小追命
+          </div>
+          <div class="phone">
+            13800000000<span class="iconfont iconright"></span>
+          </div>
+        </div>
+        <div class="uadress">
+          深圳市宝安区幸福路快乐街28号
+        </div>
+      </div>
+      <img class="img" src="../../../static/images/cart_border@2x(1).png" alt="">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      address: null
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+@import './index.less';
 </style>
--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -2,20 +2,22 @@
   <div>
     <!-- 轮播图 -->
     <swiper class="lunbo" indicator-dots autoplay>
-      <block v-for="(item, index) in detailObj" :key="index">
+      <block v-for="(item, index) in detailObj.pics" :key="index">
         <swiper-item>
-          <image :src="item" class="slide-image" mode="aspcetFill" />
+          <image :src="item.pics_big" class="slide-image" mode="aspcetFill" />
         </swiper-item>
       </block>
     </swiper>
     <!-- 商品信息 -->
     <div class="box">
-      <div class="price">￥279</div>
+      <div class="price">￥{{detailObj.goods_price}}</div>
       <div class="mess">
-        <div class="word">婴儿床杆子圆床通用支架挂杆两用杆实木杆遮光杆支架遮光架婴儿床杆子圆床通用支架挂杆两用杆实木杆遮光杆支架遮光架</div>
+        <div class="word">{{detailObj.goods_name}}</div>
         <div class="shoucang">
           <span class="iconfont icon-shoucang"></span>
-          <span @click="aa">收藏</span>
+          <span>分享
+          </span>
+          <button class="mybtn" open-type="share"></button>
         </div>
       </div>
       <div class="pass">快递：免运费</div>
@@ -23,17 +25,18 @@
     <!-- 图文介绍 -->
     <div class="jieshao">
       <div class="title">图文介绍</div>
-      <div class="msg"></div>
+      <div class="msg" v-html="detailObj.goods_introduce"></div>
     </div>
     <!-- bottomtab -->
     <div class="bottom">
       <div class="callme">
         <span class="iconfont icon-service"></span>
         <span>联系客服</span>
+        <button open-type="contact"></button>
       </div>
       <div class="cart">
         <span class="iconfont icon-gouwuche"></span>
-        <span>购物车</span>
+        <span @click="toCart">购物车</span>
       </div>
       <div class="incart">加入购物车</div>
       <div class="buy">立即购买</div>
@@ -42,29 +45,43 @@
 </template>
 
 <script>
-import request from "../../utils/request.js"
+// 引入 request 
+import request from '../../utils/request.js'
 export default {
   data() {
     return {
-      detailObj: {}
+      detailObj: {},
+      goods_id: ''
     }
   },
   methods: {
-    aa(){
+    aa() {
       wx.showShareMenu()
+    },
+    // 跳转到购物车：
+    toCart() {
+      wx.switchTab({
+        url: '/pages/cart/main'
+      })
     }
   },
- async mounted() {
-    //接受参数
-    var goods_id = this.$root.$mp.query.id
-    var url = "https://itjustfun.cn/api/public/v1/goods/detail"
-    var res = await request.get(url,{
-      goods_id:goods_id
+  async mounted() {
+    // 接收参数
+    this.goods_id = this.$root.$mp.query.id
+    var url = 'https://itjustfun.cn/api/public/v1/goods/detail'
+    var res = await request.get(url, {
+      goods_id: this.goods_id
     })
     this.detailObj = res.data.data
-    console.log(res.data.data);
-    
+    console.log(res.data.data)
   },
+  onShareAppMessage(res) {
+    return {
+      title: '震惊-据专家研究得到，烧开的水不能直接饮用',
+      path: '/pages/detail/main?goods_id=' + this.goods_id,
+      imageUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=4116997908,4289274602&fm=173&app=49&f=JPEG?w=218&h=146&s=DA2382457C36780D5522D57203009031'
+    }
+  }
 }
 </script>
 
